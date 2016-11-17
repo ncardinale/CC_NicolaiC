c3bec56f5daac2446cc50970d7913e98bc91c1b0
ncardinale <ndc309@nyu.edu>
2016-11-17 09:40:38 -0500

    Add files via upload

--- SS8_Nicolai_Cardinale/sketch.js
+++ SS8_Nicolai_Cardinale/sketch.js
@@ -1,4 +1,6 @@
 var words;
+var displayWords = [];
+
 var rankCol = 0;
 var wordCol = 1;
 var partOfSpeechCol = 2;
@@ -10,37 +12,62 @@ words = loadTable("Word_Freq_Top_100.csv", "csv", "header");
 }
 
 function setup() {
-  
   createCanvas(1000,500);
   background(200);
-  console.log(words.getRowCount() + " total rows in table");
-  console.log(words.getColumnCount() + " total columns in table");
-  console.log(words.columns);
-  //row = words.getRow(1);
+  //console.log(words.getRowCount() + " total rows in table");
+  //console.log(words.getColumnCount() + " total columns in table");
+
+  // Setup the first array
   for (var c = 0; c < 10; c++){
-    textAlign(LEFT, TOP);
-    textSize(150-c*12);
-    text(words.getString(c, wordCol), random(10,800), random(10,350));
+    row = words.getRow(c)
+    displayWords[c] = [row.getString(wordCol), row.getString(freqCol)];
   }
-  //console.log(row.getString(c));
   
+  drawWords();
 }
 
 function draw() {
 
 }
 
-function keyTyped(){
-  console.log(key);
-  
-  var rowsSelect = words.findRows(key, wordCol);
-  console.log(rowsSelect);
+function drawWords(){
   for (var c = 0; c < 10; c++){
-    //;
-    //console.log(rowsSelect.getString(c, wordCol));
-    
-    //textAlign(LEFT, TOP);
-    //textSize(150-c*12);
-    //text(words.getString(c, wordCol), random(10,800), random(10,350));
+    textAlign(LEFT, TOP);
+    textSize(150-c*12);
+    fill(0,250-c*20,0);
+    text(displayWords[c][0], random(10,800), random(10,350));
   }
+}
+
+function keyTyped(){
+
+  if (key==' '){
+    // Get 10 random words
+    for (var c = 0; c < 10; c++){
+      randomWordNum = random(0, words.getRowCount()-1);
+      randomWordNum = round(randomWordNum);
+      row = words.getRow(randomWordNum);
+      displayWords[c] = [row.getString(wordCol), row.getString(freqCol)];
+    }
+    
+    // Sort the displayWords in order of frequency
+    
+    background(200);
+    displayWords = displayWords.sort(sortFunction);
+    drawWords();
+  }
+  
+}
+
+function sortFunction(a, b) {
+  //parseInt takes string and makes it into intiger
+    if (parseInt(a[1]) === parseInt(b[1])) {
+        rtn = 0;
+    }
+    else {
+      // ?  : = turrnary operator. Mushed If statement
+        rtn = (parseInt(a[1]) < parseInt(b[1])) ? 1 : -1;
+    }
+    console.log("A = " + a + " B = " + b + " RTN = " + rtn);
+    return rtn;
 }
