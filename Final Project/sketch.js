6a3050594d154c2260acd5781073b8c9861aab25
ncardinale <ndc309@nyu.edu>
2016-12-01 10:35:39 -0500

    Add files via upload

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -12,16 +12,22 @@ var Rel = 1;
 var Sht = 2;
 var Dck = 3;
 var menuFont;
+
+var countDown = false;
+
 var currentTime = 0;
 var saveTime = 0;
+var timer = 3;
 
-
-
-var currentTime = 0;
-var savedTime = 0;
+var stageSelect = 0;
 
 var p1Choice = 0;
 var p2Choice = 0;
+var redChords = 0;
+var blueChords = 0;
+
+//Image Varubles
+var redGuy;
 
 function preload(){
   menuFont = loadFont('libraries/BroshK.ttf');
@@ -29,21 +35,29 @@ function preload(){
 
 function setup() {
   createCanvas(1000, 600);
-  time();
+  choose();
 }
 
 function draw() {
-  console.log("Player 1:" + p1Choice);
-  console.log("Player 2:" + p2Choice);
-  console.log ("ct" + currentTime);
-  console.log ("st" + saveTime);
-  
+  //console.log("Player 1:" + p1Choice);
+  //console.log("Player 2:" + p2Choice);
+  console.log ("ct: " + currentTime);
+  console.log ("st: " + saveTime);
+  console.log ("Red Tune: " + redChords);
+  console.log ("Blue Tune: " + blueChords);
+
+ if(countDown){
+   if(currentTime - saveTime > 3000){
+  console.log("Shoot");
+  saveTime = currentTime;
+  shoot();
+  countDown=false;
+  }
+ }
   currentTime = millis();
 }
 
 function MainMenu(){
-
-  
   fill('#ED225D');
   textFont(menuFont);
   textSize(150);
@@ -68,15 +82,9 @@ function MainMenu(){
   textFont(menuFont);
   textSize(69);
   text('EXIT', 450, 525);
-
-
 }
 
-function time(){
-  currentTime = millis();
-  saveTime = currentTime;
-  choose();
-}
+
 
 function choose(){
   background(200);
@@ -86,10 +94,10 @@ function choose(){
   rect(200, 400, 100, 100);
   //Player2
   rect(700, 400, 100, 100);
-  if ((currentTime - saveTime) > 3000){
-  shoot();
+  
 }
-}
+    
+ //Check if Red/Blue Chords are 0
 
 function P1Win(){
   //Player1
@@ -107,10 +115,6 @@ function P2Win(){
   //Player2
   fill(0, 255, 0);
   rect(700, 400, 100, 100);
-  //fill(0);
-  //textAlign(CENTER);
-  //textSize(100);
-  //text("Player 2 Wins", width/2 height/4);
 }
 
 function noWin(){
@@ -133,15 +137,17 @@ function noDie(){
 // choice = 3 (duck)
 
 function shoot(){
+  
   if ((p1Choice == Skip) && (p2Choice == Skip)){
     noDie();
   }
   if ((p1Choice == Skip) && (p2Choice == Rel)){
     noDie();
   }
-  if ((p1Choice == Skip) && (p2Choice == Sht)){
+  if ((p1Choice == Skip) && (p2Choice == Sht) && (blueChords > 0)){
     P2Win();
-  }
+  } else {noDie();}
+  
   if ((p1Choice == Skip) && (p2Choice == Dck)){
     noDie();
   }
@@ -151,9 +157,10 @@ function shoot(){
   if ((p1Choice == Rel) && (p2Choice == Rel)){
     noDie();
   }
-  if ((p1Choice == Rel) && (p2Choice == Sht)){
+  if ((p1Choice == Rel) && (p2Choice == Sht) && (blueChords > 0)){
     P2Win();
-  }
+  } else {noDie();}
+  
   if ((p1Choice == Rel) && (p2Choice == Dck)){
     noDie();
   }
@@ -181,16 +188,43 @@ function shoot(){
   if ((p1Choice == Dck) && (p2Choice == Dck)){
     noDie();
   }
+  
+ if (redChords < 0){
+   redChords = 0;
+ }
+ if (blueChords < 0){   
+   blueChords = 0;
+ }
+ if (redChords > 6){
+   redChords = 6;
+ }
+ if (blueChords > 6){   
+   blueChords = 6;
+ }
 }
+  
+ //if (redChords>0){
+ //}
+ //if (blueChords>0){
+ //}
+ //if (redChords=0){
+ //}
+ //if (blueChords=0){
+ //}
+ 
 
 function keyTyped(){
 
 // Player 1 choice
    if (key=='w'){
      p1Choice = 1;
+     redChords = redChords + 1;
+     
    }
    if (key=='d'){
      p1Choice = 2;
+     redChords = redChords - 1;
+
    }
    if (key=='s'){
      p1Choice = 3;
@@ -199,9 +233,13 @@ function keyTyped(){
 // Player 2 choice
    if (key=='i'){
      p2Choice = 1;
+     blueChords = blueChords + 1;
+     
    }
     if (key=='j'){
      p2Choice = 2;
+     blueChords = blueChords - 1;
+
    }
     if (key=='k'){
      p2Choice = 3;
@@ -210,13 +248,16 @@ function keyTyped(){
 //shoot!
     if (key==' '){
       shoot();
+      
     }
     if (key== '0'){
+      countDown = true;
       p1Choice = 0;
       p2Choice = 0;
+      saveTime = currentTime;
+      //stageSelect = 2;
       choose();
     }
+    
 }
-
-
 
